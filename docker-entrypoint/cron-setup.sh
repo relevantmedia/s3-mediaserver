c4ea5af29797d812ef98c07158a63a7988646f05
Ryan Strandt <ryan.strandt@cru.org>
2016-02-01 10:42:16 -0500

    cron setup

--- docker-entrypoint/cron-setup.sh
+++ docker-entrypoint/cron-setup.sh
@@ -3,6 +3,6 @@
 # Check Configuration
 S3_BACKUP_BUCKET=$S3_BACKUP_BUCKET
 
-if [ -z "$S3_BACKUP_BUCKET" ] then
+if [ -z "$S3_BACKUP_BUCKET" ]; then
   mv /root/.aws/s3-upload.sh /etc/cron.daily/
 fi
